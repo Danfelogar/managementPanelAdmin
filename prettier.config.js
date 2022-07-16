f0655fa579525389e85b38989bca688e1abe6447
Danfelogar <danypolo18@hotmail.com>
2022-07-15 21:42:28 -0500

    make instances of ESlint and Prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    endOfLine: 'auto',
+    printWidth: 130,
+    trailingComma: 'all',
+    tabWidth: 4,
+    bracketSpacing: true,
+    arrowParens: 'always',
+}
