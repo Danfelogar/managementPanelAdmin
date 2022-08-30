29d7d52b2058fd4daa4f5523e0a83f6b6b3c20e4
Danfelogar <danypolo18@hotmail.com>
2022-08-30 00:21:40 -0500

    error in graphicsBar

--- hooks/pages/useGraphics.ts
+++ hooks/pages/useGraphics.ts
@@ -1,12 +1,70 @@
-import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
 
-export const useGraphics = () => {
-    const { data, status } = useSession()
+interface AuxGraphics {
+    frecuencia_de_reparacion: number
+    porcentaje_de_disponibilidad: number
+    createdAt: string
+}
+
+interface Props {
+    frecuencia_de_reparacion: number
+    porcentaje_de_disponibilidad: number
+    frecuencia_de_falla: number
+    createdAt?: string
+}
+
+export const useGraphics = (graphics: AuxGraphics[]) => {
+    const [arrGraphics, setArrGraphics] = useState<AuxGraphics[] | Props[]>(graphics)
+    const [isLoading, setIsLoading] = useState(true)
+    let auxArr: Props[] = []
+
+    const mappingArrGraphics = () => {
+        setIsLoading(true)
+        for (let i = 0; i < graphics.length; i++) {
+            const fdr = graphics[i].frecuencia_de_reparacion
+            const pdd = graphics[i].porcentaje_de_disponibilidad
+
+            auxArr[graphics[i].createdAt] ??= []
+            auxArr[graphics[i].createdAt] = {
+                frecuencia_de_reparacion: (auxArr[graphics[i].createdAt].frecuencia_de_reparacion || 0) + fdr,
+                frecuencia_de_falla: (auxArr[graphics[i].createdAt].frecuencia_de_falla || 0) + 1,
+                porcentaje_de_disponibilidad: (auxArr[graphics[i].createdAt].porcentaje_de_disponibilidad || 0) + pdd,
+            }
+
+            //console.log(`iteracion ${i}:`, auxArr)
+        }
+        //console.log({ auxArr })
+        // if (auxArr.length > 0) {
+        //     for (let i = 0; i < auxArr.length; i++) {
+        //         const fdr = auxArr[i].frecuencia_de_reparacion
+        //         const pdd = auxArr[i].porcentaje_de_disponibilidad
+        //         const fdf = auxArr[i].frecuencia_de_falla
+
+        //         auxArr[graphics[i].createdAt] = {
+        //             frecuencia_de_reparacion: fdr / fdf,
+        //             frecuencia_de_falla: fdf,
+        //             porcentaje_de_disponibilidad: pdd / fdf,
+        //         }
+        //     }
+        // }
+        if (auxArr.length === 13) {
+            setArrGraphics(auxArr as Props[])
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        console.log(arrGraphics.length)
+        if (arrGraphics.length > 0) {
+            mappingArrGraphics()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return {
         //states
-        data,
-        status,
+        arrGraphics,
+        isLoading,
         //methods
         //functions
     }
--- pages/graphics/[id].tsx
+++ pages/graphics/[id].tsx
@@ -3,20 +3,36 @@ import { EngineeringOutlined } from '@mui/icons-material'
 import { GetServerSideProps } from 'next'
 import moment from 'moment'
 
-import { AdminLayout, CustomBar } from '../../components'
+import { AdminLayout, CustomBar, Loading } from '../../components'
 import { ITheme, ISeguimiento } from '../../interface'
 import { WrapperGraphicsBar } from '../../components/ui/styles/styledGraphicsBar'
 import { useGraphics } from '../../hooks'
 import { dbGraphics } from '../../database'
 
-const BarGraphicPage: NextPage<ITheme> = ({ toggleTheme }) => {
-    const { status } = useGraphics()
+interface AuxGraphics {
+    frecuencia_de_reparacion: number
+    porcentaje_de_disponibilidad: number
+    createdAt: string
+}
+interface Props extends ITheme {
+    graphics: AuxGraphics[]
+    id_maquina: string
+    auxArr?: any
+}
+
+const BarGraphicPage: NextPage<Props> = ({ graphics, toggleTheme, id_maquina, auxArr }) => {
+    const { arrGraphics, isLoading } = useGraphics(graphics)
+
+    console.log({ graphics })
+    if (isLoading) {
+        return <Loading size={'70px'} title={`Cargando Gráficos, por favor espere...`} toggleTheme={toggleTheme} />
+    }
 
     return (
         <AdminLayout
             icon={<EngineeringOutlined color="secondary" />}
             subTitle={'Visor de IND'}
-            title={'Inventario No.12'}
+            title={`Inventario No.${id_maquina}`}
             toggleTheme={toggleTheme}
         >
             <WrapperGraphicsBar>
@@ -31,7 +47,7 @@ const BarGraphicPage: NextPage<ITheme> = ({ toggleTheme }) => {
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
     // console.log(ctx.query?.id!)
-    const graphicsByMachine = await dbGraphics.getFollowByMachineId(ctx.query?.id!.toString())
+    const graphicsByMachine: ISeguimiento[] = await dbGraphics.getFollowByMachineId(ctx.query?.id!.toString())
 
     if (graphicsByMachine.length === 0) {
         return {
@@ -41,40 +57,75 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
             },
         }
     }
+    // let auxArr:
+    //     | {
+    //           frecuencia_de_reparacion: number
+    //           porcentaje_de_disponibilidad: number
+    //           frecuencia_de_falla: number
+    //           createdAt?: string
+    //       }[]
+    //     | [] = []
+    let graphics:
+        | {
+              frecuencia_de_reparacion: number
+              porcentaje_de_disponibilidad: number
+              createdAt: string
+          }[]
+        | [] = []
 
-    let graphics = graphicsByMachine.flatMap((item) => [
-        {
-            ...item,
-            frecuencia_de_reparacion: Math.round((1 / item.tiempoDeReparacion) * 1000) / 1000,
+    graphics = graphicsByMachine.reduce((acc: any, currentValue: any) => {
+        let auxObj = {}
+
+        auxObj = {
+            frecuencia_de_reparacion: Math.round((1 / currentValue.tiempoDeReparacion) * 1000) / 1000,
             porcentaje_de_disponibilidad:
                 Math.round(
-                    (item.tiempoDeFuncionamiento +
-                        item.tiempoDeReparacion +
+                    (currentValue.tiempoDeFuncionamiento +
+                        currentValue.tiempoDeReparacion +
                         5 -
-                        item.tiempoDeReparacion / (item.tiempoDeFuncionamiento + item.tiempoDeReparacion + 5)) *
+                        currentValue.tiempoDeReparacion /
+                            (currentValue.tiempoDeFuncionamiento + currentValue.tiempoDeReparacion + 5)) *
                         1000,
                 ) / 1000,
-            createdAt: moment(item.createdAt).utcOffset('+0500').format('DD/MM/YYYY').split('/')[1],
-        },
-    ])
+            createdAt: moment(currentValue.createdAt).utcOffset('+0500').format('DD/MM/YYYY').split('/')[1],
+        }
 
-    console.log({ graphics })
+        return [...acc, auxObj]
+    }, [])
 
-    let auxArr: Partial<ISeguimiento[]> | [] = []
+    graphics = graphics.reduce((acc: any, currentValue: any) => {
+        let auxObj = {}
+        let auxArr = acc
+        const fdr = currentValue.frecuencia_de_reparacion
+        const pdd = currentValue.porcentaje_de_disponibilidad
 
-    for (let i = 0; i < graphics.length; i++) {
-        const frecuencia_de_reparacion = graphics[i].frecuencia_de_reparacion
-        const porcentaje_de_disponibilidad = graphics[i].porcentaje_de_disponibilidad
+        auxArr[currentValue.createdAt] ??= []
+        auxArr[currentValue.createdAt] = {
+            frecuencia_de_reparacion: (auxArr[currentValue.createdAt].frecuencia_de_reparacion || 0) + fdr,
+            frecuencia_de_falla: (auxArr[currentValue.createdAt].frecuencia_de_falla || 0) + 1,
+            porcentaje_de_disponibilidad: (auxArr[currentValue.createdAt].porcentaje_de_disponibilidad || 0) + pdd,
+        }
 
-        auxArr[graphics[i].createdAt] ??= []
-        auxArr[graphics[i].createdAt].push({ frecuencia_de_reparacion, porcentaje_de_disponibilidad })
-    }
+        return [...acc, auxObj]
+    }, [])
 
-    console.log({ auxArr })
-    console.log(auxArr['12'])
+    // for (let i = 0; i < graphics.length; i++) {
+    //     const fdr = graphics[i].frecuencia_de_reparacion
+    //     const pdd = graphics[i].porcentaje_de_disponibilidad
+
+    //     auxArr[graphics[i].createdAt] ??= []
+    //     auxArr[graphics[i].createdAt] = {
+    //         frecuencia_de_reparacion: (auxArr[graphics[i].createdAt].frecuencia_de_reparacion || 0) + fdr,
+    //         frecuencia_de_falla: (auxArr[graphics[i].createdAt].frecuencia_de_falla || 0) + 1,
+    //         porcentaje_de_disponibilidad: (auxArr[graphics[i].createdAt].porcentaje_de_disponibilidad || 0) + pdd,
+    //     }
+
+    //     // console.log(`iteracion ${i}:`, auxArr)
+    // }
+    // console.log(auxArr)
 
     return {
-        props: {},
+        props: { graphics, id_maquina: ctx.query?.id!.toString() },
     }
 }
 
